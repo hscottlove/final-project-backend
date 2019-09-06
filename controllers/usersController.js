43f99eabedc402608a08b45f32b60a09c07f340c
Scottlove <haleyscottlove@gmail.com>
2019-09-06 11:36:12 -0700

    added user controller show db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,13 @@
 const db = require('../models');
 
-// const show = (req, res) => {
-//     db.User.findById(req)
-// };
+const show = (req, res) => {
+  db.User.findById(req.session.currentUser.id, { password: 0, __v: 0 },  (err, foundUser) => {
+    if (err) return res.status(500).json({ status: 500, message: 'Please try again.' });
+
+    res.status(200).json({ status: 200, data: foundUser });
+  });
+};
+
+module.exports = {
+  show,
+};
