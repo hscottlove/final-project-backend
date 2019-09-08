154dcb8dcc9918654a915d18a60ebdcf21154e93
Scottlove <haleyscottlove@gmail.com>
2019-09-07 17:19:49 -0700

    relinked post route in server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', function (req, res) {
 // SECTION Routes
 app.use('/api/v1/auth', routes.auth);
 app.use('/api/v1/users', routes.users);
-// app.use('/api/v1/posts', routes.posts);
+app.use('/api/v1/posts', routes.posts);
 
 // SECTION Server Listen
 app.listen(PORT, () => console.log(`Server now connected to PORT ${PORT}!`))
